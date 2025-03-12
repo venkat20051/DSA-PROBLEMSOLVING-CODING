a1ab57a65de875930bece30190f82db72b61b5b5
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-03-12 10:17:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Geometric Progression Term/geometric-progression-term.java
@@ -0,0 +1,20 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int n = scn.nextInt();
+        int r = 2;
+        // Compute and Print
+        System.out.print(a<<(n-1));
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
