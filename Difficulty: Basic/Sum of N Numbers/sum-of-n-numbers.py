9aac9afd18562cf219e470651e4745873d09d012
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-04-20 00:04:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sum of N Numbers/sum-of-n-numbers.py
@@ -0,0 +1,15 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+n = int(input())
+# Your code here
+print((n*(n+1))//2)
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
