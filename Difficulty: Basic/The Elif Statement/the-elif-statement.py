3f8ad1a91e6928b9783311e903805f76ea312ace
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-04-23 23:06:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The Elif Statement/the-elif-statement.py
@@ -0,0 +1,20 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+
+# } Driver Code Ends
+
+#User function Template for python3
+n = int(input())
+if n>100:
+    print("Big")
+elif n<10:
+    print("Small")
+else:
+    print("Number")
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
