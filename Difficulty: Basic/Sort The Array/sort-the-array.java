bfe5426f520eba8c8bbae97ff26739ba67658045
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-04-27 10:01:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sort The Array/sort-the-array.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = Integer.parseInt(scanner.nextLine().trim());
+
+        while (t-- > 0) {
+            String line = scanner.nextLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            Solution obj = new Solution();
+            obj.sortArr(nums);
+            for (int x : nums) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+class Solution {
+    void sortArr(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+    }
+}
