dd2daeb02053f008592f5affc53aebe70fa1fa9a
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-04-28 22:48:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Divisor/divisor.java
@@ -0,0 +1,22 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            if(n%i==0)
+            System.out.print(i+" ");
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
