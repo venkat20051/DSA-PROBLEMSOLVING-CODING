870d3dd9fd473f9ac22acfc615c623f36a74dfeb
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-05-02 21:45:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/For Loop - 1/for-loop-1.java
@@ -0,0 +1,21 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        for(int i=n;i<=n*10;i=i+n)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
