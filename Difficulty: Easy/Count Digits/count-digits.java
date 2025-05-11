c034a41983966e4747fa1457a2d338c74cf13c6f
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-05-11 20:59:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -0,0 +1,20 @@
+//{ Driver Code Starts
+
+// } Driver Code Ends
+
+//Back-end complete function Template for Java
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int ans=(int)Math.log10(n)+1;
+        System.out.println(ans);
+    }
+}
+
+
+//{ Driver Code Starts.
+// } Driver Code Ends
