1220ab0a50cc9ef3bcc74020d2f7f44a18a50c6d
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-05-13 22:45:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multi Printing/multi-printing.py
@@ -0,0 +1,18 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+# } Driver Code Ends
+
+#User function Template for python3
+
+a = input()
+n = int(input())
+#Write your code below that prints a n times
+for i in range(0,n):
+    print(a,end="")
+
+
+#{ 
+ # Driver Code Starts.
+# } Driver Code Ends
