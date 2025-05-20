06211578c36416def190518e0e1fa1622ae9cce7
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-05-20 22:30:09 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/The FizzBuzz Program/the-fizz-buzz-program.py
@@ -0,0 +1,10 @@
+#User function Template for python3
+n=int(input())
+if(n%3==0 and n%5==0):
+    print("FizzBuzz")
+elif(n%3==0):
+    print("Fizz")
+elif(n%5==0):
+    print("Buzz")
+else:
+    print(n)
