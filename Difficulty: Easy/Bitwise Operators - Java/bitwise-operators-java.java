86ef9fd1c0b2b3c38edf061d1d20e77a45284151
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-07-06 22:34:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Bitwise Operators - Java/bitwise-operators-java.java
@@ -0,0 +1,14 @@
+// Function to perform bitwise manipulations
+// a, b and c are input integers
+class Geeks {
+    static void bitWiseOp(int a, int b, int c) {
+
+        // Your code here
+         System.out.println(a^a);
+    System.out.println(c^b);
+    System.out.println(a&b);
+    System.out.println(c|(a^a));
+    System.out.println(~(c^b));
+        
+    }
+}
