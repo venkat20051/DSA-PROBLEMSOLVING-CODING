a195f892db256d51fe05f1b04c1562f3e4c395d9
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-07-13 18:44:30 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Size of an Array/size-of-an-array.java
@@ -0,0 +1,15 @@
+// User function Template for Java
+
+class Solution {
+    private int[] a;
+
+    // Constructor to initialize the array
+    public Solution(int[] arr) { this.a = arr; }
+
+    // Method to get the size of the array
+    public int getSize() {
+        // Your code here
+        
+        return a.length;
+    }
+}
