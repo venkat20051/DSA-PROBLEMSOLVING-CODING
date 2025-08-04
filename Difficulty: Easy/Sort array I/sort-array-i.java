ca76e4b6fab622013f6d20072fe9e74aac49a90e
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-08-04 21:04:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort array I/sort-array-i.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void sortArr(int[] arr) {
+        // code here
+        for(int i=0;i<arr.length;i++)
+        {
+            for(int j=i+1;j<arr.length;j++)
+            {
+                if(arr[i]>arr[j])
+                {
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+            
+        }
+    }
