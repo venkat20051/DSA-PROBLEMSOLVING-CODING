06d2c9a1538e6c3b91a7c6c16d3836a969d3cfae
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-09-14 23:57:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Square wall/print-square-wall.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String args[]) {
+        // Your Code Here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println("");
+        }
+    }
+}
