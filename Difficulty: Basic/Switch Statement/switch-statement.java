64584cdcb2194f8bf62b8afdfb274f543b1275bd
kotha Venkat <149996618+venkat20051@users.noreply.github.com>
2025-09-22 20:57:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Switch Statement/switch-statement.java
@@ -0,0 +1,41 @@
+class Solution {
+    public void solve() {
+        // code here
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        switch(a)
+        {
+        case 1:
+            System.out.println("One");
+            break;
+        case 2:
+            System.out.println("Two");
+            break;
+        case 3:
+            System.out.println("Three");
+            break;
+        case 4:
+            System.out.println("Four");
+            break;
+        case 5:
+            System.out.println("Five");
+            break;
+        case 6:
+            System.out.println("Six");
+            break;
+        case 7:
+            System.out.println("Seven");
+            break;
+        case 8:
+            System.out.println("Eight");
+            break;
+        case 9:
+            System.out.println("Nine");
+            break;
+        default:
+        System.out.println("Unknown");
+        break;
+        }
+            
+    }
+}
